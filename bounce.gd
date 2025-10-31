f4fbf13b5d82dc661be1c59d1e930dbf688ee208
OliveADay <sharinggon23@gmail.com>
2025-10-30 21:22:40 -0700

    s

--- bounce.gd
+++ bounce.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 var rng :RandomNumberGenerator = RandomNumberGenerator.new()
+var mouse_in  =false
+signal clicked_on
 
 
 
@@ -9,3 +11,16 @@ func _ready() -> void:
 	var lin =  Vector2(x_lin, y_lin)*100
 	set_linear_velocity(lin)
 	
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("click")):
+		clicked_on.emit()
+	
+
+	
+
+
+func _on_mouse_entered() -> void:
+	mouse_in = true
+
+func _on_mouse_exited() -> void:
+	mouse_in = false
--- world.gd
+++ world.gd
@@ -8,13 +8,35 @@ const Y_MAX = 345
 const X_MIN = 15
 const X_MAX = 625
 var gui_num = 2
+var guis = []
+var count = 0
 
 func spawn():
+	for gui in guis:
+		gui.queue_free()
+	guis = []
 	var i = 0
+	if(gui_num > 2):
+		count+=1
+		$CanvasLayer/Label.text = "score: "+str(count)
 	while(i < gui_num):
 		i+=1
 		var pos = Vector2(rng.randi_range(X_MIN, X_MAX), rng.randi_range(Y_MIN,Y_MAX))
-		#TODO
-		#get spawning done, and have last one be friend
+		if(i == gui_num):
+			var fren = friend_ref.instantiate()
+			add_child(fren)
+			fren.position = pos
+			fren.clicked_on.connect(spawn)
+			guis.append(fren)
+			print("fren spawned")
+		else:
+			var gui = base_ref.instantiate()
+			add_child(gui)
+			gui.position = pos
+			guis.append(gui)
+	gui_num+=1
+			
+func _ready() -> void:
+	spawn()
 			
 	
